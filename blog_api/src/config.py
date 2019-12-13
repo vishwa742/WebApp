81e892ff0181f2e36ab5ffd0b2be75098a6ee866
vishwanath7 <ezhilvishwanath@gmail.com>
2019-12-13 00:55:00 -0500

    Updated part 3 with testing class

--- blog_api/src/config.py
+++ blog_api/src/config.py
@@ -39,3 +39,12 @@ app_config = {
     'production': Production,
     'testing': Testing
 }
+
+class Testing(object):
+    """
+    Development environment configuration
+    """
+    TESTING = True
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
