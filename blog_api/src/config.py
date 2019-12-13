986fa9bc65b71359096b045b1ef77b7351851b77
vishwanath7 <ezhilvishwanath@gmail.com>
2019-12-13 00:58:20 -0500

    Feature: Populated tests folder

--- blog_api/src/config.py
+++ blog_api/src/config.py
@@ -39,12 +39,3 @@ app_config = {
     'production': Production,
     'testing': Testing
 }
-
-class Testing(object):
-    """
-    Development environment configuration
-    """
-    TESTING = True
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_TEST_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
